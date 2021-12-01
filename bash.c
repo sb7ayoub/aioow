89dfc0f46d229adba5e206996b0f895560d64fec
sb7ayoub <95380984+sb7ayoub@users.noreply.github.com>
2021-12-01 21:05:02 +0100

    Add files via upload

---
+++ bash.c
@@ -0,0 +1,29 @@
+#!/bin/bash
+arc=$(uname -a)
+pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+pdisk=$(df -h --total | tail -1 | awk '{printf "%s/%s (%s)\n", $3, $2, $5}')
+cpul=$(mpstat | tail -1 | awk '{printf "%s(%%)\n", $4+$6}')
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lvmt=$(lsblk | grep "lvm" | wc -l)
+lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
+ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+ulog=$(users | wc -w)
+ip=$(hostname -I)
+mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
+cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+wall "	#Architecture: $arc
+	#CPU physical : $pcpu
+	#vCPU: $vcpu
+	#Memory Usage: $uram/${fram}MB ($pram%)
+	#Disk Usage: $pdisk
+	#CPU load: $cpul
+	#Last boot: $lb
+	#LVM use: $lvmu
+	#Connexions TCP: $ctcp
+	#User log: $ulog
+	#Network IPv4 $ip ($mac)
+	#Sudo: $cmds cmd"
